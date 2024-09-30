c2fc24e517b9a3d2a404d75d2f883b30fbc41bd3
Ayush <ayushaggarwal42003@gmail.com>
2024-09-30 13:32:21 +0530

    Some Changes

--- src/modules/builder/nav-bar/components/TemplateSelect.tsx
+++ src/modules/builder/nav-bar/components/TemplateSelect.tsx
@@ -24,7 +24,7 @@ export const TemplateSelect = () => {
       <div>
         <OutlinedButton
           onClick={() => {
-            window.open('https://github.com/sadanandpai/resume-builder', '_blank');
+            window.open('https://github.com/Ayush40/resume-builder.git', '_blank');
           }}
         >
           Contribute on Github
